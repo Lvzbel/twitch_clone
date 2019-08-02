e56347cca4b7c817063a4c3d49866792b7c13618
Rodrigo <rcoto@protonmail.com>
2019-08-01 23:09:39 -0700

    Steams Reducer completed

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { reducer as formReduser } from "redux-form";
 import authReducer from "./authReducer";
+import streamReducer from "./streamReducer";
 
 export default combineReducers({
   auth: authReducer,
-  form: formReduser
+  form: formReduser,
+  streams: streamReducer
 });
